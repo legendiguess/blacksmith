275234e4b4cb7917964aee761987eece7fe5af33
OggyKUN <46613040+OggyKUN@users.noreply.github.com>
2020-08-09 22:04:28 +0300

    refactor: motion to motion_x

--- scenes/character/Character.gd
+++ scenes/character/Character.gd
@@ -3,17 +3,17 @@ extends Node2D
 const MAX_SPEED = 300
 const ACCELERATION = 100
 
-var motion = 0
+var motion_x = 0
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed('ui_right'):
 		$Sprite.flip_h = false
-		motion = min(motion + ACCELERATION, MAX_SPEED)
+		motion_x = min(motion_x + ACCELERATION, MAX_SPEED)
 	elif Input.is_action_pressed('ui_left'):
-		motion = max(motion - ACCELERATION, -MAX_SPEED)
+		motion_x = max(motion_x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 	else:
-		motion = lerp(motion, 0, 0.2)
+		motion_x = lerp(motion_x, 0, 0.2)
 		
-	position.x += motion * delta
+	position.x += motion_x * delta
