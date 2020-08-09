1bc67c9ff814e650e924745be885f508e8a1a157
OggyKUN <46613040+OggyKUN@users.noreply.github.com>
2020-08-09 21:44:39 +0300

    refactor: change Vector2 to pos.x

--- scenes/character/Character.gd
+++ scenes/character/Character.gd
@@ -3,18 +3,17 @@ extends Node2D
 const MAX_SPEED = 300
 const ACCELERATION = 100
 
-var motion = Vector2()
+var motion = 0
 
 func _physics_process(delta):
 	
-	motion = Vector2(motion.x, 0)
 	if Input.is_action_pressed('ui_right'):
 		$Sprite.flip_h = false
-		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		motion = min(motion + ACCELERATION, MAX_SPEED)
 	elif Input.is_action_pressed('ui_left'):
-		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		motion = max(motion - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 	else:
-		motion.x = lerp(motion.x, 0, 0.2)
+		motion = lerp(motion, 0, 0.2)
 		
-	position.x += motion.x * delta
+	position.x += motion * delta
