a786ea64cf1998f2ce4b5074a8326afe3cb2d0ee
snodack <computer.zhen@gmail.com>
2020-08-14 15:49:17 +0300

    feat: add interaction with the chest

--- scenes/chest/Chest.gd
+++ scenes/chest/Chest.gd
@@ -11,23 +11,51 @@ var items_button = [] # Array of buttons
 
 #Code sector
 func _ready():
-	items.resize(N)
-#	for i in range (N):
-#		items[i] = Item.new()
-	items[0] = item_factory.new_item(item_table.ids.copper_ore)
+	items.append(item_factory.new_item(item_table.ids.copper_ore))
+	items.append(item_factory.new_item(item_table.ids.copper_ore))
 	log_out()
 	pass 
 
 func log_out():
+	for i in items_button:
+		i.queue_free()
+	items_button.clear()
 	for i in items:
 		if i == null: continue
 		var btn = Button.new()
 		$VBoxContainer.add_child(btn)
-		btn.text = i.log_out()
 		btn.connect("button_down", self, "_on_click_item_button", [btn])
+		btn.text = i.log_out()
 		items_button.append(btn)
 
-func _on_click_item_button(btn):
-	var i = items_button.find(btn)
-	print(i)
+func put(item) -> bool:
+	if items.size() < N:
+		items.append(item)
+		update()
+		return true
+	else: return false
 	
+func take(item_index):
+	if $"/root/Inventory".current_item != null:
+		var item_hand = $"/root/Inventory".current_item
+		$"/root/Inventory".current_item = items[item_index]
+		items[item_index] = item_hand
+	else:
+		$"/root/Inventory".current_item = items[item_index]
+		items.remove(item_index)
+		
+	$"/root/Inventory".update()
+	update()
+		
+	
+func _on_click_item_button(btn):
+	take(items_button.find(btn))
+
+func _on_Button_button_up():
+	var item = $"/root/Inventory".current_item
+	if item != null && put(item):
+		 $"/root/Inventory".put()
+	pass # Replace with function body.
+
+func update():
+	log_out()
--- scenes/inventory/Inventory.gd
+++ scenes/inventory/Inventory.gd
@@ -4,17 +4,22 @@ extends Node
 var Item = load("res://scenes/items/Base_item.gd")
 
 #Declare Variables
-var N:int = 1  # Number of slots
-var items = [] # Array of items
+var current_item # Array of items
 
 #Code sector
 func _ready():
-	
-	log_out()
-	pass 
+	update()
+	 
 
+func update():
+	log_out()
+	
 func log_out():
 	var st = "Inventory: \n"
-	for i in items:
-		st+=(i.log_out() +"\n")
+	if current_item !=null:
+		st+=(current_item.log_out() +"\n")
 	$LogText.text = st
+
+func put():
+	current_item = null
+	log_out()
