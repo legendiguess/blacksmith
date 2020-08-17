7860f56d153bd20a86581c9937b1fc1aacda736a
gostovskyy <computer.zhen@gmail.com>
2020-08-17 19:37:27 +0300

    feat: add dwarf controller

--- scenes/strings/Strings.gd
+++ scenes/strings/Strings.gd
@@ -1,12 +1,15 @@
 extends Node
 # Массив строк зависит от языка
 onready var item_table = get_node("/root/ItemTable")
-var names
-func _ready():
-	names = {
+onready var names = {
 		item_table.Ids.COPPER_ORE : "Copper ore",
+		item_table.Ids.GOLDEN_ORE : "Golden ore",
+		item_table.Ids.IRON_ORE : "Iron ore",
 		item_table.Ids.FANTASIUM_ORE : "Fantasium ore",
 		item_table.Ids.COPPER_INGOT : "Copper ingot"
 	}
-	pass
+func _ready():
 	
+	pass
+
+
