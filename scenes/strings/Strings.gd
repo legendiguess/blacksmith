a50aa5665714af1f74b8cf30213598850897f25f
legendiguess <legendiguess@mail.ru>
2020-08-21 06:43:28 +0300

    feat: add translations to ru and eu

--- scenes/strings/Strings.gd
+++ scenes/strings/Strings.gd
@@ -2,14 +2,17 @@ extends Node
 # Массив строк зависит от языка
 onready var item_table = get_node("/root/ItemTable")
 onready var names = {
-		item_table.Ids.COPPER_ORE : "Copper ore",
-		item_table.Ids.GOLDEN_ORE : "Golden ore",
-		item_table.Ids.IRON_ORE : "Iron ore",
-		item_table.Ids.FANTASIUM_ORE : "Fantasium ore",
-		item_table.Ids.COPPER_INGOT : "Copper ingot"
+		item_table.Ids.COPPER_ORE : tr("COPPER_ORE"),
+		item_table.Ids.IRON_ORE : tr("IRON_ORE"),
+		item_table.Ids.SILVER_ORE : tr("SILVER_ORE"),
+		item_table.Ids.GOLDEN_ORE : tr("GOLDEN_ORE"),
+		item_table.Ids.FANTASIUM_ORE : tr("FANTASIUM_ORE"),
+		item_table.Ids.COPPER_INGOT : tr("COPPER_INGOT"),
+		item_table.Ids.IRON_INGOT : tr("IRON_INGOT"),
+		item_table.Ids.SILVER_INGOT : tr("SILVER_INGOT"),
+		item_table.Ids.GOLDEN_INGOT: tr("GOLDEN_INGOT"),
+		item_table.Ids.FANTASIUM_INGOT: tr("FANTASIUM_INGOT"),
 	}
+
 func _ready():
-	
 	pass
-
-
