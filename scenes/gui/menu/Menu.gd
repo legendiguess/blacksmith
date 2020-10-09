257ac0c75bad8fdfbbe7f98eb1d610b5c583aad8
legendiguess <legendiguess@mail.ru>
2020-10-09 20:18:14 +0300

    feat: add `use_quantity` flag to turn off/on quantity in items menu

--- scenes/gui/menu/Menu.gd
+++ scenes/gui/menu/Menu.gd
@@ -5,11 +5,16 @@ onready var preload_item_button = preload("res://scenes/gui/menu/ItemButton.tscn
 
 signal item_menu_picked(item_button)
 
+export var use_quantity: bool
+
 func add_item_button(icon, item_name, item_quantity):
 	var new_item_button = preload_item_button.instance()
 	new_item_button.icon = icon
 	new_item_button.text = item_name
-	new_item_button.get_node("Quantity").text = str(item_quantity)
+	if use_quantity:
+		new_item_button.get_node("Quantity").text = str(item_quantity)
+	else:
+		new_item_button.get_node("Quantity").queue_free()
 	$VBoxContainer.add_child(new_item_button)
 
 func remove_item_button_by_item_name(item_name):
