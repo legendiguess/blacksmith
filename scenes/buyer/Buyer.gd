0b556cad83445282763d81695749a5faf89b4d5b
gostovskyy <computer.zhen@gmail.com>
2020-10-11 22:23:06 +0300

    add test event

--- scenes/buyer/Buyer.gd
+++ scenes/buyer/Buyer.gd
@@ -3,8 +3,9 @@ extends Node2D
 var order # id оружия
 var sprites = []#Разные типы спрайтов
 var phrase = "" 
-func init(id):
+func init(id, _phrase):
 	order = id
+	phrase = _phrase
 
 func say():
 	return (phrase)
--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _ready():
+	init(1, load("res://scenes/events/test_event.gd").new())
 	pass
 func init(number, event):
 	$"Сashier".set_event(event)
