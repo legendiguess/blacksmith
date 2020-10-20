5ec8ecbaa1d215f178ad514071d5cfa47360de90
legendiguess <legendiguess@mail.ru>
2020-10-20 09:38:04 +0300

    refactor: remove log text feature from character inventory

--- scenes/character/Inventory.gd
+++ scenes/character/Inventory.gd
@@ -12,13 +12,6 @@ func update():
 		get_node("../ItemInHandsSprite").texture = null
 	else:
 		get_node("../ItemInHandsSprite").texture = current_item.sprites
-	log_out()
-
-func log_out():
-	var st = "Inventory: \n"
-	if current_item !=null:
-		st+=(current_item.log_out() +"\n")
-	$LogText.text = st
 
 func put():
 	current_item = null
