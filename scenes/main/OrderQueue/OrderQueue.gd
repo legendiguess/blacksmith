016282e84cd0ee2b06570a1a87de76b0f21c3d1b
legendiguess <legendiguess@mail.ru>
2020-11-07 23:33:24 +0300

    fix: remove shadowing of order variable in OrderQueue

--- scenes/main/OrderQueue/OrderQueue.gd
+++ scenes/main/OrderQueue/OrderQueue.gd
@@ -14,8 +14,8 @@ func new_buyer_come(buyer):
 	add(ItemFactory.new_item(buyer.order), "%s %s" % [buyer.say(), Strings.names[buyer.order]])
 
 func buyer_left(buyer_index):
-	var order = get_children()[buyer_index]
-	while order.modulate.a > 0:
+	var order_that_need_to_remove = get_children()[buyer_index]
+	while order_that_need_to_remove.modulate.a > 0:
 		yield(get_tree(), "idle_frame")
-		order.modulate.a -= 0.01
-	order.queue_free()
+		order_that_need_to_remove.modulate.a -= 0.01
+	order_that_need_to_remove.queue_free()
