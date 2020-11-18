afaaa37449e5085dcb30cd5ef15612da581a2680
legendiguess <legendiguess@mail.ru>
2020-11-19 02:23:30 +0300

    feat: add original pitch for audio player script

--- scenes/sounds/AudioStreamPlayer2D.gd
+++ scenes/sounds/AudioStreamPlayer2D.gd
@@ -1,9 +1,10 @@
 extends AudioStreamPlayer2D
 
 export var audio_streams: Array = []
+onready var original_pitch = self.pitch_scale
 
 func play(from_position: float = 0.0):
 	if audio_streams.size() > 0:
 		stream = audio_streams[int(rand_range(0, audio_streams.size()))]
-	pitch_scale = 1 + rand_range(-0.1, 0.1)
+	self.pitch_scale = original_pitch + rand_range(-0.1, 0.1)
 	.play(from_position)
